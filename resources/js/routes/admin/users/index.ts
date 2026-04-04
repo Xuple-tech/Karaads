6845fdab8cd795c62f938d8daff51eaa91dc63d4
Duke Of Bot Leg <emmanueloyibo74@gmail.com>
2026-04-04 14:47:47 +0100

    before clean up

--- resources/js/routes/admin/users/index.ts
+++ resources/js/routes/admin/users/index.ts
@@ -382,4 +382,9 @@ const users = {
 create: Object.assign(create, create),
 store: Object.assign(store, store),
 show: Object.assign(show, show),
-edit: Object.assign(edi
+edit: Object.assign(edit, edit),
+update: Object.assign(update, update),
+destroy: Object.assign(destroy, destroy),
+}
+
+export default users
